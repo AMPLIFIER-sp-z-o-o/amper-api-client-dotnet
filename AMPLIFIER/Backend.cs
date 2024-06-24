6b2e3ac2521a3c5c5b437ef357f4f9378d946497
Tomek Dziemidowicz <tomek@dziemidowicz.cloud>
2024-06-24 10:51:38 +0200

    feat: sales-representatives-import

--- AMPLIFIER/Backend.cs
+++ AMPLIFIER/Backend.cs
@@ -1762,5 +1762,34 @@ namespace Amplifier
                 await CreateLogEntryAsync(LogSeverity.Error, e.Message, e);
             }
         }
+        
+        public async System.Threading.Tasks.Task SendSalesRepresentativesAsync(List<SalesRepresentative> sales_representatives)
+        {
+            try
+            {
+                await ValidateJWTToken();
+                await CreateLogEntryAsync(LogSeverity.Info, "About to send " + sales_representatives.Count() + " SalesRepresentatives.");
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await _client.PostAsync(_wsConfig.B2BWSUrl + "sales-representatives-import",
+                    new StringContent(JsonConvert.SerializeObject(sales_representatives), Encoding.UTF8, "application/json"));
+                if (!response.IsSuccessStatusCode)
+                {
+                    watch.Stop();
+                    await CreateLogEntryAsync(LogSeverity.Error,
+                        "FAILURE while sending SalesRepresentatives after " + watch.ElapsedMilliseconds + " ms; "
+                        + await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    watch.Stop();
+                    await CreateLogEntryAsync(LogSeverity.Info,
+                        "Success while sending SalesRepresentatives after " + watch.ElapsedMilliseconds + " ms.");
+                }
+            }
+            catch (Exception e)
+            {
+                await CreateLogEntryAsync(LogSeverity.Error, e.Message, e);
+            }
+        }        
     }
 }
---
+++ AMPLIFIER/SalesRepresentative.cs
@@ -0,0 +1,13 @@
+namespace Amplifier
+{
+    public class SalesRepresentative
+    {
+        public string identifier { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string keycloak_id { get; set; }
+        public string supervisor { get; set; }
+    }
+}
