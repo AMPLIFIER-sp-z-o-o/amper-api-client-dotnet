c9d7c36cfccef9a02c33a88722d667d8922c2fc4
Tomek Dziemidowicz <tomek@dziemidowicz.cloud>
2025-07-23 18:59:48 +0200

    Updates package version and adds a property
    Increments the package version to 1.1.148.
    
    Adds a boolean property to the Settlement class that indicates whether the item can be paid via the payment gateway.

--- AMPLIFIER/Settlement.cs
+++ AMPLIFIER/Settlement.cs
@@ -12,5 +12,6 @@ namespace Amplifier
         public string date { get; set; }
         public string due_date { get; set; }        
         public string external_id { get; set; }        
+        public bool can_be_paid_via_the_payment_gateway { get; set; } = false;      
     }
 }
